066bef71399786aedd6b76686a71565f0c3ceae9
Radiat09 <rzsianboyridoy10@gmail.com>
2025-07-29 22:04:25 +0600

    change generating tracking id method with nanoid for uniqueness

--- src/app/utils/generateTrackingId.ts
+++ src/app/utils/generateTrackingId.ts
@@ -1,5 +1,7 @@
+import { nanoid } from "nanoid";
+
 export const generateTrackingId = (): string => {
   const datePart = new Date().toISOString().replace(/-/g, "").slice(0, 8); // YYYYMMDD
-  const randomPart = Math.random().toString(36).substring(2, 8).toUpperCase(); // 6 random chars
-  return `TRK-${datePart}-${randomPart}`;
+  const uniqueId = nanoid(6).toUpperCase(); // 6-character random string (e.g., "A1B2C3")
+  return `TRK-${datePart}-${uniqueId}`;
 };
