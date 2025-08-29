36182215566d7c64c27b07d371b5485f17ebf2ad
Radiat09 <rzsianboyridoy10@gmail.com>
2025-08-29 20:51:53 +0600

    added trust proxy,express urlencoded and cors origin to app.ts file

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
@@ -16,30 +14,26 @@ const globalErrorHandler_1 = require("./app/middlewares/globalErrorHandler");
 const notFound_1 = __importDefault(require("./app/middlewares/notFound"));
 const routes_1 = require("./app/routes");
 const app = (0, express_1.default)();
-app.use(
-  (0, express_session_1.default)({
+app.use((0, express_session_1.default)({
     secret: "Your secreet",
     resave: false,
     saveUninitialized: false,
-  })
-);
+}));
 app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
 app.set("trust proxy", 1);
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use(
-  (0, cors_1.default)({
+app.use((0, cors_1.default)({
     origin: env_1.envVars.FRONTEND_URL,
     credentials: true,
-  })
-);
+}));
 app.use("/api/v1", routes_1.router);
 app.get("/", (req, res) => {
-  res.status(200).json({
-    message: "Welcome to Parcel Delivery System backend",
-  });
+    res.status(200).json({
+        message: "Welcome to Parcel Delivery System backend",
+    });
 });
 app.use(globalErrorHandler_1.globalErrorHandler);
 app.use(notFound_1.default);
