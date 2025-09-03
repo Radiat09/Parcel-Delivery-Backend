08ec4ede4b0452d4ae721cbff50eadf0a472ccf6
Radiat Hossain Ridoy <rzsianboyridoy10@gmail.com>
2025-09-04 01:08:27 +0600

    Get me (#33)
    * add vercel.json file
    
    * first build
    
    * create parcel interface
    
    * modified parcel interfaces
    
    * Parcel module (#4)
    
    * first build
    
    * create parcel interface
    
    * modified parcel interfaces
    
    * added parcel validation file with a parcel presave hook for generating a unique tracking id
    
    * change generating tracking id method with nanoid for uniqueness
    
    * create parcel route ,controller and service created
    
    * add delivary history logic
    
    * get all parcel for both sender and admins with different response for both
    
    * added email zod validation in reciver of parcel and handled reciver get all parcel
    
    * second biuld
    
    * Parcel module (#5)
    
    * first build
    
    * create parcel interface
    
    * modified parcel interfaces
    
    * added parcel validation file with a parcel presave hook for generating a unique tracking id
    
    * change generating tracking id method with nanoid for uniqueness
    
    * create parcel route ,controller and service created
    
    * add delivary history logic
    
    * get all parcel for both sender and admins with different response for both
    
    * added email zod validation in reciver of parcel and handled reciver get all parcel
    
    * second biuld
    
    * second build 2.3
    
    * second build 2.6
    
    * Parcel module (#8)
    
    * first build
    
    * create parcel interface
    
    * modified parcel interfaces
    
    * added parcel validation file with a parcel presave hook for generating a unique tracking id
    
    * change generating tracking id method with nanoid for uniqueness
    
    * create parcel route ,controller and service created
    
    * add delivary history logic
    
    * get all parcel for both sender and admins with different response for both
    
    * added email zod validation in reciver of parcel and handled reciver get all parcel
    
    * second biuld
    
    * Development (#6)
    
    * add vercel.json file
    
    * Parcel module (#4)
    
    * first build
    
    * create parcel interface
    
    * modified parcel interfaces
    
    * Parcel module (#5)
    
    * first build
    
    * create parcel interface
    
    * modified parcel interfaces
    
    * added parcel validation file with a parcel presave hook for generating a unique tracking id
    
    * change generating tracking id method with nanoid for uniqueness
    
    * create parcel route ,controller and service created
    
    * add delivary history logic
    
    * get all parcel for both sender and admins with different response for both
    
    * added email zod validation in reciver of parcel and handled reciver get all parcel
    
    * second biuld
    
    * second build 2.3
    
    * second build 2.6
    
    * second build 2.9
    
    * parcel module finish
    
    * parcel module finish
    
    * parcel module finishing touch
    
    * add readme.md file
    
    * final build
    
    * final build 2.2
    
    * final build 2.3
    
    * modify readme.md file
    
    * added trust proxy,express urlencoded and cors origin to app.ts file
    
    * added trust proxy,express urlencoded and cors origin to app.ts file
    
    * added get-me route,controller and service
    
    * added second option to get accesToken in checkauth middleware
    
    * added express session screet from .env, added samesite in setCookie functon
    
    * added adding or removing field option function to query builder
    
    * added populate when getting all parcel data of sender field

--- dist/app/utils/generateTrackingId.js
+++ dist/app/utils/generateTrackingId.js
@@ -2,9 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.generateTrackingId = void 0;
 const nanoid_1 = require("nanoid");
+// export const generateTrackingId = (): string => {
+//   const datePart = new Date().toISOString().replace(/-/g, "").slice(0, 8); // YYYYMMDD
+//   const uniqueId = nanoid(6).toUpperCase(); // 6-character random string (e.g., "A1B2C3")
+//   return `TRK-${datePart}-${uniqueId}`;
+// };
 const generateTrackingId = () => {
-    const datePart = new Date().toISOString().replace(/-/g, "").slice(0, 8); // YYYYMMDD
-    const uniqueId = (0, nanoid_1.nanoid)(6).toUpperCase(); // 6-character random string (e.g., "A1B2C3")
+    const datePart = new Date().toISOString().replace(/-/g, "").slice(0, 8);
+    const uniqueId = (0, nanoid_1.nanoid)(8).toUpperCase(); // Increase to 8 characters
     return `TRK-${datePart}-${uniqueId}`;
 };
 exports.generateTrackingId = generateTrackingId;
