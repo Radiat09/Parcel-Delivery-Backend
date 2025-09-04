cc0195fdacab5c2d3e87fd9c2ba69bcf4830dd5b
Radiat Hossain Ridoy <rzsianboyridoy10@gmail.com>
2025-09-04 18:17:07 +0600

    Get me (#36)
    * add vercel.json file
    
    * first build
    
    * create parcel interface
    
    * modified parcel interfaces
    
    * Parcel module (#4)
    
    * first build
    
    * create parcel interface
    
    * modified parcel interfaces
    
    * added parcel validation file with a parcel presave hook for generating a unique tracking id
    
    * change generating tracking id method with nanoid for uniqueness
    
    * create parcel route ,controller and service created
    
    * add delivary history logic
    
    * get all parcel for both sender and admins with different response for both
    
    * added email zod validation in reciver of parcel and handled reciver get all parcel
    
    * second biuld
    
    * Parcel module (#5)
    
    * first build
    
    * create parcel interface
    
    * modified parcel interfaces
    
    * added parcel validation file with a parcel presave hook for generating a unique tracking id
    
    * change generating tracking id method with nanoid for uniqueness
    
    * create parcel route ,controller and service created
    
    * add delivary history logic
    
    * get all parcel for both sender and admins with different response for both
    
    * added email zod validation in reciver of parcel and handled reciver get all parcel
    
    * second biuld
    
    * second build 2.3
    
    * second build 2.6
    
    * Parcel module (#8)
    
    * first build
    
    * create parcel interface
    
    * modified parcel interfaces
    
    * added parcel validation file with a parcel presave hook for generating a unique tracking id
    
    * change generating tracking id method with nanoid for uniqueness
    
    * create parcel route ,controller and service created
    
    * add delivary history logic
    
    * get all parcel for both sender and admins with different response for both
    
    * added email zod validation in reciver of parcel and handled reciver get all parcel
    
    * second biuld
    
    * Development (#6)
    
    * add vercel.json file
    
    * Parcel module (#4)
    
    * first build
    
    * create parcel interface
    
    * modified parcel interfaces
    
    * Parcel module (#5)
    
    * first build
    
    * create parcel interface
    
    * modified parcel interfaces
    
    * added parcel validation file with a parcel presave hook for generating a unique tracking id
    
    * change generating tracking id method with nanoid for uniqueness
    
    * create parcel route ,controller and service created
    
    * add delivary history logic
    
    * get all parcel for both sender and admins with different response for both
    
    * added email zod validation in reciver of parcel and handled reciver get all parcel
    
    * second biuld
    
    * second build 2.3
    
    * second build 2.6
    
    * second build 2.9
    
    * parcel module finish
    
    * parcel module finish
    
    * parcel module finishing touch
    
    * add readme.md file
    
    * final build
    
    * final build 2.2
    
    * final build 2.3
    
    * modify readme.md file
    
    * added trust proxy,express urlencoded and cors origin to app.ts file
    
    * added trust proxy,express urlencoded and cors origin to app.ts file
    
    * added get-me route,controller and service
    
    * added second option to get accesToken in checkauth middleware
    
    * added express session screet from .env, added samesite in setCookie functon
    
    * added adding or removing field option function to query builder
    
    * added populate when getting all parcel data of sender field
    
    * fix populate at sender field
    
    * minus some fields from populate get all parcels
    
    * fix merge conflict
    
    * modified some things in popolate in get all user service

--- dist/app/modules/parcel/parcel.service.js
+++ dist/app/modules/parcel/parcel.service.js
@@ -68,59 +68,47 @@ const createParcelService = (payload) =>
     }
     const parcel = yield parcel_model_1.Parcel.create(payload);
     return parcel;
-  });
-const getAllParcelService = (query, user) =>
-  __awaiter(void 0, void 0, void 0, function* () {
-    if (
-      user.role === user_interface_1.Role.ADMIN ||
-      user.role === user_interface_1.Role.SUPER_ADMIN
-    ) {
-      const queryBuilder = new QueryBuilder_1.QueryBuilder(
-        parcel_model_1.Parcel.find(),
-        query
-      );
-      const users = queryBuilder
-        .search(parcel_constant_1.parcelSearchableFields)
-        .filter()
-        .sort()
-        .fields()
-        .paginate();
-      const [data, meta] = yield Promise.all([
-        users
-          .build()
-          .populate("sender", "name email phone")
-          .select("-sender.id -sender._id -_id"),
-        queryBuilder.getMeta(),
-      ]);
-      return {
-        data,
-        meta,
-      };
-    } else {
-      const userExist = yield user_model_1.User.findOne({ email: user.email });
-      let data;
-      if (!userExist) {
-        throw new AppError_1.AppError(
-          http_status_codes_1.default.NOT_FOUND,
-          "Invalid User request!"
-        );
-      }
-      if (user.role === user_interface_1.Role.SENDER) {
-        // Now `userExist` is a single document (or null)
-        data = yield parcel_model_1.Parcel.find({ sender: userExist._id });
-      } else {
-        data = yield parcel_model_1.Parcel.find({
-          "receiver.email": userExist.email,
-        });
-      }
-      return {
-        data,
-        meta: {
-          total: data === null || data === void 0 ? void 0 : data.length,
-        },
-      }; // Maintain consistent return type
+});
+
+const getAllParcelService = (query, user) => __awaiter(void 0, void 0, void 0, function* () {
+    if (user.role === user_interface_1.Role.ADMIN || user.role === user_interface_1.Role.SUPER_ADMIN) {
+        const queryBuilder = new QueryBuilder_1.QueryBuilder(parcel_model_1.Parcel.find(), query);
+        const users = queryBuilder
+            .search(parcel_constant_1.parcelSearchableFields)
+            .filter()
+            .sort()
+            .fields()
+            .selectField("-_id")
+            .paginate();
+        const [data, meta] = yield Promise.all([
+            users
+                .build()
+                .populate("sender", "name email phone")
+                .select("-sender.id -sender._id"),
+            queryBuilder.getMeta(),
+        ]);
+        return {
+            data,
+            meta,
+        };
     }
+    else {
+        const userExist = yield user_model_1.User.findOne({ email: user.email });
+        let data;
+        if (!userExist) {
+            throw new AppError_1.AppError(http_status_codes_1.default.NOT_FOUND, "Invalid User request!");
+        }
+        if (user.role === user_interface_1.Role.SENDER) {
+            // Now `userExist` is a single document (or null)
+            data = yield parcel_model_1.Parcel.find({ sender: userExist._id });
+        }
+        else {
+            data = yield parcel_model_1.Parcel.find({ "receiver.email": userExist.email });
+        }
+        return { data, meta: { total: data === null || data === void 0 ? void 0 : data.length } }; // Maintain consistent return type
   });
+
+
 const updateParcelService = (req) =>
   __awaiter(void 0, void 0, void 0, function* () {
     const user = req.user;
